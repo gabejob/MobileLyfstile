d9864e6f1681f06d03d4ddd189739181d2c009e3
gabee <u1028866@umail.utah.edu>
2022-03-30 20:45:08 -0600

    Fully hooked up intro screens, still need to configure to use mvvm

--- app/src/main/java/com/example/lyfstile/CameraFrag.kt
+++ app/src/main/java/com/example/lyfstile/CameraFrag.kt
@@ -1,10 +1,20 @@
 package com.example.lyfstile
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import java.lang.Exception
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +26,69 @@ private const val ARG_PARAM2 = "param2"
  * Use the [CameraFrag.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CameraFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CameraFrag : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val REQUEST_IMAGE_CAPTURE = 1
+    var profileImage: Bitmap? = null
+    private var dataList = HashMap<String, Data>()
+    private var lyfViewModel : LyfViewModel ?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_camera, container, false)
+        lyfViewModel = ViewModelProvider( this).get(LyfViewModel::class.java)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val takePictureButton = view.findViewById<Button>(R.id.Camera)
+        takePictureButton.setOnClickListener(this)
+
+        val next_button = view.findViewById<Button>(R.id.next_button)
+        next_button.setOnClickListener(this)
+        return view
+    }
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.Camera -> {
+                try {
+                    startActivityForResult(
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE),
+                        REQUEST_IMAGE_CAPTURE
+                    )
+                    addNextButton();
+                } catch (e: Exception) {
+                    throw Exception("Unable to start the camera")
                 }
             }
+            R.id.next_button -> {
+                if (profileImage == null) {
+                    val toast = Toast.makeText(
+                        requireActivity(),
+                        "No photo was taken, please take a photo",
+                        Toast.LENGTH_SHORT
+                    )
+                    toast.show()
+                } else {
+                    view?.let {
+                        Navigation.findNavController(it)
+                            .navigate(R.id.action_cameraFrag_to_reviewScreenFrag)
+                    }
+                }
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
+            val extras = data?.extras as Bundle
+            profileImage = extras.get("data") as Bitmap
+            val mIvPic = view?.findViewById<ImageView>(R.id.iv_pic)
+            mIvPic?.setImageBitmap(profileImage)
+        }
+    }
+    private fun addNextButton() {
+        view?.findViewById<Button>(R.id.Camera)?.text = "Retake Photo"
+        view?.findViewById<Button>(R.id.next_button)?.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/example/lyfstile/ReviewScreenFrag.kt
+++ app/src/main/java/com/example/lyfstile/ReviewScreenFrag.kt
@@ -1,10 +1,17 @@
 package com.example.lyfstile
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.Navigation
+import java.lang.Exception
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,44 +23,55 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ReviewScreenFrag.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ReviewScreenFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+class ReviewScreenFrag : Fragment(), View.OnClickListener {
+    private var createAccountButton: Button? = null
+    private var editButton: Button? = null
+    private var lyfViewModel : LyfViewModel ?= null
+    private var dataList : HashMap<String, Data> ?=null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review_screen, container, false)
-    }
+        val view=inflater.inflate(R.layout.fragment_review_screen, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReviewScreenFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReviewScreenFrag().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        createAccountButton = view.findViewById(R.id.create_button)
+        createAccountButton?.setOnClickListener(this)
+
+        editButton = view.findViewById(R.id.edit_button)
+        editButton?.setOnClickListener(this)
+        setInfoValues()
+        return view
+    }
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.create_button -> {
+                try {
+                    view?.let {
+                        Navigation.findNavController(it)
+                            .navigate(R.id.action_reviewScreenFrag_to_homeScreenFrag)
+                    }
+                } catch (e: Exception) {
+                    throw Exception("Unable to start the camera")
                 }
             }
+            R.id.edit_button -> {
+                view?.let {
+                    Navigation.findNavController(it)
+                        .navigate(R.id.action_reviewScreenFrag_to_cameraFrag)
+                }
+
+            }
+        }
+    }
+    private fun setInfoValues() {
+    //@todo: Pull from viemodel instead
+    //        findViewById<ImageView>(R.id.imageView)?.setImageBitmap(dataList?.get(PROFILE_PIC)?.data as Bitmap)
+//        findViewById<TextView>(R.id.first_name_box)?.text = dataList?.get(FIRST_NAME)?.data.toString()
+//        findViewById<TextView>(R.id.last_name_box)?.text = dataList?.get(LAST_NAME)?.data.toString()
+//        findViewById<TextView>(R.id.age_box)?.text = dataList?.get(AGE)?.data.toString()
+//        findViewById<TextView>(R.id.sex_box)?.text = dataList?.get(SEX)?.data.toString()
+//        findViewById<TextView>(R.id.height_box)?.text = dataList?.get(HEIGHT)?.data.toString()
+//        findViewById<TextView>(R.id.weight_box)?.text = dataList?.get(WEIGHT)?.data.toString()
     }
 }
