0d311b8944078ac3c4cb3d5750e4c1a498b166ee
Alex Romero <alexlr36@live.com>
2022-04-08 02:11:56 -0600

    Back button implementation with frags. Still has some bugs.

--- app/src/main/java/com/example/lyfstile/NewUserViewModel.kt
+++ app/src/main/java/com/example/lyfstile/NewUserViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModel
 
 class NewUserViewModel : ViewModel() {
 
+    var user = MutableLiveData<User>()
     val currentScreen =  MutableLiveData<String>() //"first_last_name"
 
     init {
         currentScreen.value = FIRST_LAST_NAME_SCREEN
+        user.value = User()
     }
 }
