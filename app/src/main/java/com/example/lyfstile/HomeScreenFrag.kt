a0deded7cebd492830bc618cbee837cc9342a8f6
jordanmiller <miller.k.jordan@gmail.com>
2022-04-19 20:07:51 -0600

    Changed Settings page to cancel and save (doesn't actually save yet)

--- app/src/main/java/com/example/lyfstile/HomeScreenFrag.kt
+++ app/src/main/java/com/example/lyfstile/HomeScreenFrag.kt
@@ -1,12 +1,16 @@
 package com.example.lyfstile
 
+import android.content.Context
 import android.graphics.BitmapFactory
+import android.hardware.*
 import android.os.Bundle
 import android.view.*
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import java.util.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -15,16 +19,27 @@ import androidx.navigation.Navigation
  */
 class HomeScreenFrag : Fragment(), ActionbarFragment.ClickInterface {
     lateinit var viewModel: LyfViewModel
+    lateinit var mSensorManager: SensorManager
+    lateinit var mLinearAccelerometer: Sensor
+    private val mThreshold = 0.5
+    private var last_x: Double = 0.0
+    private var last_y: Double = 0.0
+    private var last_z: Double = 0.0
+    private var now_x: Double = 0.0
+    private var now_y: Double = 0.0
+    private var now_z: Double = 0.0
+    private var mNotFirstTime = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
 
         viewModel = ViewModelProvider(requireActivity())[LyfViewModel::class.java]
 
-        viewModel.getUser(requireActivity(), viewModel.user.email)!!.observe(viewLifecycleOwner){
+        viewModel.getUser(requireActivity(), viewModel.user.email)!!.observe(viewLifecycleOwner) {
             viewModel.user = User(it)
         }
 
@@ -37,8 +52,68 @@ class HomeScreenFrag : Fragment(), ActionbarFragment.ClickInterface {
         fragtrans.commit()
         actionbarFragment.bindClickInterface(this)
 
-        view.findViewById<ImageView>(R.id.pfp_box)?.setImageBitmap(BitmapFactory.decodeByteArray(viewModel.user.pfp, 0,viewModel.user.pfp.size))
+        view.findViewById<ImageView>(R.id.pfp_box)?.setImageBitmap(
+            BitmapFactory.decodeByteArray(
+                viewModel.user.pfp,
+                0,
+                viewModel.user.pfp.size
+            )
+        )
+
+        mSensorManager = context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        //Get the default light sensor
+        mLinearAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+
+
         return view
+    }
+
+    private val mListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(sensorEvent: SensorEvent) {
+
+            //Get the acceleration rates along the y and z axes
+            now_x = sensorEvent.values[0].toDouble()
+            now_y = sensorEvent.values[1].toDouble()
+            now_z = sensorEvent.values[2].toDouble()
+            if (mNotFirstTime) {
+                val dx: Double = Math.abs(last_x - now_x)
+                val dy: Double = Math.abs(last_y - now_y)
+                val dz: Double = Math.abs(last_z - now_z)
+
+                //Check if the values of acceleration have changed on any pair of axes
+                if (dx > mThreshold && dy > mThreshold ||
+                    dx > mThreshold && dz > mThreshold ||
+                    dy > mThreshold && dz > mThreshold
+                ) {
+
+                    // Start step counter
+                    val rnd = Random()
+                    print("***********************$rnd***********************")
+                }
+            }
+            last_x = now_x
+            last_y = now_y
+            last_z = now_z
+            mNotFirstTime = true
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(
+            mListener,
+            mLinearAccelerometer,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(mListener)
     }
 
     override fun actionButtonClicked(id: Int) {
--- app/src/main/java/com/example/lyfstile/MainActivity.kt
+++ app/src/main/java/com/example/lyfstile/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.lyfstile
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -12,18 +11,17 @@ import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.nio.channels.FileChannel
 
-class MainActivity : AppCompatActivity(){
+
+class MainActivity : AppCompatActivity() {
     private var lyfViewModel: LyfViewModel? = null
-    interface Backup
-    {
+
+    interface Backup {
         fun bind(context: Context)
         fun downloadDB()
         fun uploadFile()
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -51,25 +49,26 @@ class MainActivity : AppCompatActivity(){
         lyfViewModel = ViewModelProvider(this)[LyfViewModel::class.java]
         lyfViewModel!!.bind(this)
     }
-    fun downloadDB()
-    {
+
+    fun downloadDB() {
         val db = File("/data/data/com.example.lyfstile/databases/user_db")
         Amplify.Storage.downloadFile("db", db,
             { Log.i("MyAmplifyApp", "Successfully downloaded: ${it.file.name}") },
-            { Log.e("MyAmplifyApp",  "Download Failure", it) }
+            { Log.e("MyAmplifyApp", "Download Failure", it) }
         )
         val dbwal = File("/data/data/com.example.lyfstile/databases/user_db-wal")
         Amplify.Storage.downloadFile("dbwal", dbwal,
             { Log.i("MyAmplifyApp", "Successfully downloaded: ${it.file.name}") },
-            { Log.e("MyAmplifyApp",  "Download Failure", it) }
+            { Log.e("MyAmplifyApp", "Download Failure", it) }
         )
         val dbshm = File("/data/data/com.example.lyfstile/databases/user_db-shm")
         Amplify.Storage.downloadFile("dbshm", dbshm,
             { Log.i("MyAmplifyApp", "Successfully downloaded: ${it.file.name}") },
-            { Log.e("MyAmplifyApp",  "Download Failure", it) }
+            { Log.e("MyAmplifyApp", "Download Failure", it) }
         )
 
     }
+
     fun uploadFile() {
         var pathdb = getDatabasePath("user_db").absolutePath
         var pathdbshm = getDatabasePath("user_db-shm").absolutePath
@@ -95,7 +94,6 @@ class MainActivity : AppCompatActivity(){
             { Log.e("MyAmplifyApp", "Upload failed", it) }
         )
     }
-
 
 
 }
